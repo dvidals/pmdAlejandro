577922e416c9c123950e1376650f0d145bd9d75d
Alejandro Vidal Domínguez <avidaldo@ATEIS.LOCAL>
2020-11-06 17:43:43 +0100

    clase 06

--- 1-CicloDeVida/app/src/main/java/com/example/a1_ciclodevida/MainActivity.java
+++ 1-CicloDeVida/app/src/main/java/com/example/a1_ciclodevida/MainActivity.java
@@ -19,12 +19,13 @@ public class MainActivity extends AppCompatActivity { //AppCompatActivity hereda
         sb.append(linea +"\n"); // Añade la línea recibida por parámetro al Stringbuilder
         tv = findViewById(R.id.tv1); // Busca el elemento con id "tv1" en la clase R (generada automáticamente a partir de los xml)
         tv.setText(sb.toString()); // Setea el contenido del TextView con el contenido actualizado del StringBuilder
-        Log.i("Ciclo de vida: ", linea); // Log.i con "i" de info, podría ser w(arning), e(rror)...
+        Log.i("CicloDeVida", linea); // Log.i con "i" de info, podría ser w(arning), e(rror)...
     }
 
     @Override // Sobreescribimos los métodos de Activity
     protected void onCreate(Bundle savedInstanceState) { // Se lanza cuando se crea la Actividad
         super.onCreate(savedInstanceState); // Invoca al onCreate de la clase padre
+
         setContentView(R.layout.activity_main);
 
         sb = new StringBuilder(""); // Inicializa el StringBuilder para empezar a añadir contenido
