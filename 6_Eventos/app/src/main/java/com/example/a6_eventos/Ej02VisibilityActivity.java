bd56ca15833854a8112994802363f9308fd070a8
Alejandro Vidal Domínguez <avidaldo@ateis.local>
2021-02-03 16:07:33 +0100

    gestión botón atrás

--- 6_Eventos/app/src/main/java/com/example/a6_eventos/Ej02VisibilityActivity.java
+++ 6_Eventos/app/src/main/java/com/example/a6_eventos/Ej02VisibilityActivity.java
@@ -1,7 +1,9 @@
 package com.example.a6_eventos;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -13,6 +15,24 @@ public class Ej02VisibilityActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ej02_visibility);
+
+        /* Para gestionar el comportamiento del botón "Atrás":
+         https://developer.android.com/guide/navigation/navigation-custom-back#java
+         */
+        getOnBackPressedDispatcher().addCallback(this,
+                new OnBackPressedCallback(true) {
+                    @Override
+                    public void handleOnBackPressed() {
+                        // Indicamos que el botón atrás iniciará una nueva MainActivity
+                        startActivity(new Intent(Ej02VisibilityActivity.this, MainActivity.class));
+                        // Además, la activity actual se finaliza (de ese modo,
+                        // si tras pasar a MainActivity pulsamos otra vez el botón "Atrás",
+                        // no regresa a ésta.
+                        Ej02VisibilityActivity.this.finish();
+                    }
+                }
+        );
+
     }
 
     public void onToggleClicked(View view) {
@@ -25,4 +45,6 @@ public class Ej02VisibilityActivity extends AppCompatActivity {
             ((ImageView) findViewById(R.id.iv_mira_aqui)).setVisibility(View.INVISIBLE);
         }
     }
+
+
 }
