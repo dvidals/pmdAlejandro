097fb4ba765c15bd90860f95f0455920dc36a864
avidaldo <avd.formacion@gmail.com>
2021-03-03 14:49:35 +0100

    cambios P3 previo

--- 7_P2_Eventos/app/src/main/java/com/example/a7_p2_eventos/Ej03Activity.java
+++ 7_P2_Eventos/app/src/main/java/com/example/a7_p2_eventos/Ej03Activity.java
@@ -8,12 +8,18 @@ import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
 public class Ej03Activity extends AppCompatActivity {
+
+
+    private static final DecimalFormat decimalFormat = new DecimalFormat("#.##");
 
     /* Las conversiones entre divisan cambian cada día, así que no se podría definir una fija.
     Una aplicación real debería conectarse a Internet y solicitar la conversión a alguna API.
      */
-    private static final float EUR_POR_DOLLAR = 0.83f;
+    private static final double EUR_POR_DOLLAR = 0.83;
 
 
     TextView tvConversion;
@@ -35,23 +41,29 @@ public class Ej03Activity extends AppCompatActivity {
 
     public void click03(View view) {
 
-        try {
-            float valor = Float.valueOf(etValor03.getText().toString());
 
-            float valorConvertido;
+        try {
+            double valor = Double.parseDouble(etValor03.getText().toString());
+
+            if (BigDecimal.valueOf(valor).scale() > 2) {
+                tvConversion.setText("Una cifra en esa divisa no debe tener más de dos decimales.");
+                return;
+            }
+
+            double valorConvertido;
 
             if (rgConversion.getCheckedRadioButtonId() == R.id.rbDolEur) {
                 valorConvertido = valor * EUR_POR_DOLLAR;
-                tvConversion.setText(valor + "$ equivalen a " + valorConvertido + "€");
+                tvConversion.setText(valor + " $ equivalen a " + decimalFormat.format(valorConvertido) + " €");
             } else if (rgConversion.getCheckedRadioButtonId() == R.id.rbEurDol) {
                 valorConvertido = valor / EUR_POR_DOLLAR;
-                tvConversion.setText(valor + "€ equivalen a " + valorConvertido + "$");
+                tvConversion.setText(valor + " .55€ equivalen a " + decimalFormat.format(valorConvertido) + " $");
             } else {
                 tvConversion.setText("Debe seleccionar una conversión");
             }
 
         } catch (NumberFormatException e) {
-            tvConversion.setText("Debe introducir un valor");
+            tvConversion.setText("Debe introducir un valor numérico");
             return;
         }
 
